d5e482e01d2b95c7cad9e6e5940f3286773a76f0
Laura <40493346+Laurapaws@users.noreply.github.com>
2022-11-19 21:41:34 +0000

    Abandon hope all ye who enter

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from matplotlib.widgets import Slider
 
 class Weapon:
     instances = []
-    def __init__(self, name, colour, optimal, falloff_mod, DPS, plot_type='area'):
+    def __init__(self, name, colour, optimal, falloff_mod, DPS, line_object=None, plot_type='line'):
         self.name = name
         self.colour = colour
         self.optimal = optimal
@@ -13,6 +13,7 @@ class Weapon:
         self.DPS = DPS
         self.plot_type = plot_type
         self.result = []
+        self.line_object = line_object
         self.__class__.instances.append(self)
         
     def calc_damage(self, distance_series):
@@ -31,14 +32,13 @@ class SimulationProperties:
         self.max_range = max_range
         self.steps = steps
         self.falloff_intensity = falloff_intensity
-    
-def calculate_single(optimal, falloff_modifier, distance_to_target):
-    
-    falloffCount = max(0, ( (distance_to_target - optimal) / optimal) * (1 / falloff_modifier))  
-    damageModifier = (sim.falloff_intensity ** (falloffCount ** 2))
+
+# class SlideController:
+#     def __init__(self, slider=None, axis=None):
+#         self.slider = slider
+#         self.axis = self.slider.axis
         
-    return damageModifier
-    
+        
 def init_data_dict(min_range, max_range, steps):
     
     interval = (max_range - min_range) / steps
@@ -47,31 +47,39 @@ def init_data_dict(min_range, max_range, steps):
     
     return data_dict, range_series
 
-def init_plots(data_dict):    
-    df = pd.DataFrame(data_dict)
-    ax = plt.gca() 
-
 def plot_data(data_dict, weapon_list):
     
-    df = pd.DataFrame(data_dict)
-    ax = plt.gca()
-    
+    fig, ax = plt.subplots()
+      
     for weapon in weapon_list:
-        df.plot(kind = weapon.plot_type,
-                x = 'ranges',
-                y = weapon.name,
-                color = weapon.colour,
-                alpha = 0.4,
-                ax = ax
-                )
-    
+        weapon.line_object, = plt.plot(data_dict["ranges"], data_dict[weapon.name], label = weapon.name)
+        
     plt.title('Kurator')
-    plt.show()
+    
+    return fig, ax
 
 def setup_data_dict(data_dict, range_series):
         for weapon in Weapon.instances:
+            data_dict[weapon.name] = []
+            weapon.result = []
             data_dict[weapon.name] = weapon.calc_damage(range_series)
         return data_dict
+
+def update_values():
+    setup_data_dict(data_dict, range_series)
+    for weapon in Weapon.instances:
+        weapon.line_object.set_ydata(data_dict[weapon.name])    
+    fig.canvas.draw_idle()
+
+def update_falloff_intensity(val):
+    sim.falloff_intensity = val
+    update_values()
+    
+# def set_slider(name, pos_tuple, axis, valmin, valmax, valinit):
+#     ax_control = plt.axes(pos_tuple)
+#     falloff_slider = Slider(ax_control, name, valmin, valmax, valinit=valinit)
+    
+    
     
 
 if __name__ == "__main__":
@@ -79,9 +87,23 @@ if __name__ == "__main__":
     sim = SimulationProperties(5, 15000, 500, 0.8)
     data_dict, range_series = init_data_dict(sim.min_range, sim.max_range, sim.steps)
 
-
-    autocannon = Weapon('Autocannon', 'red', 2000, 3, 30, 'line')
+    autocannon = Weapon('Autocannon', 'red', 2000, 3, 30)
     burst_laser = Weapon('Burst_Laser', 'black', 6000, 0.2, 22.9)
     
-    print("test")
-    plot_data(setup_data_dict(data_dict, range_series), Weapon.instances)
+    # slide_controllers = []
+    
+    #set_slider('Falloff Intensity:', [0.2, 0.05, 0.6, 0.03], ax_control, 0, 1, sim.falloff_intensity)
+    ax_control = plt.axes([0.2, 0.05, 0.6, 0.03])
+    falloff_slider = Slider(ax_control, 'Falloff Intensity:', 0, 1, valinit=sim.falloff_intensity)
+    
+    # slide_controllers.append(SlideController(
+    #     Slider(ax_control, 'Falloff Intensity:', 0, 1, valinit=sim.falloff_intensity), 
+    #     plt.axes([0.2, 0.05, 0.6, 0.03]) 
+    #     ))
+    
+    fig, ax = plot_data(setup_data_dict(data_dict, range_series), Weapon.instances)
+    
+    falloff_slider.on_changed(update_falloff_intensity)
+    
+    plt.show()
+    
